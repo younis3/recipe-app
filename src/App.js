edd31e75b8c5cbc28c70356d123bef7d3ac017df
younis3 <ahmady00003@gmail.com>
2021-12-09 18:13:12 +0200

    deploy 2

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   }, [query])
 
   const getRecipes = async () => {
-    const res = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${process.env.APP_ID}&app_key=${process.env.API_KEY}`);
+    const res = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_API_KEY}`);
     const data = await res.json();
     console.log(data);
     setRecipes(data.hits);
