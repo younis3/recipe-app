132eb93cc8ed3fe897712fb3dc66467ff48c6ee3
younis3 <ahmady00003@gmail.com>
2021-12-09 18:44:11 +0200

    improved style and added demo link in readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Recipe REACT App
 
-### The goal is to practice REACT and work with Edamam API
+#### The goal is to practice REACT and work with Edamam API
+
+### Demo Link: https://demorecipeapp.netlify.app/
--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -7,12 +7,12 @@ const Recipe = ({ title, calories, ingredients, imgSRC, link }) => {
             <h2 className={style['title']}>{title}</h2>
             <p className={style['calories']}>Total Calories: {calories}</p>
             <img className={style['img']} src={imgSRC} alt="" />
-            <ul className={style['ingredients']}>ingredients:
+            <ul className={style['ingredients']}><span className={style['ingredient-title']}>ingredients:</span>
                 {ingredients.map(ingredient => (
                     <li>{ingredient.text}</li>
                 ))}
             </ul>
-            <a className={style['link']} href={link} target="_blank">Open Recipe Details</a>
+            <button className={style['link-btn']}><a className={style['link']} href={link} target="_blank">Open Recipe Details</a></button>
         </div>
     )
 }
