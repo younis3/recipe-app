38143532161b5c260a6eb1c2f34a0f4356674635
younis3 <ahmady00003@gmail.com>
2021-12-09 16:56:43 +0200

    style

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -1,19 +1,21 @@
 import React from 'react'
-import './Recipe.css';
+import style from './recipe.module.css'
 
-const Recipe = ({ title, calories, ingredients, imgSRC }) => {
+const Recipe = ({ title, calories, ingredients, imgSRC, link }) => {
     return (
-        <div>
-            <h2>{title}</h2>
-            <p>{calories}</p>
-            <ol>
+        <div className={style['recipe']}>
+            <h2 className={style['title']}>{title}</h2>
+            <p className={style['calories']}>Total Calories: {calories}</p>
+            <img className={style['img']} src={imgSRC} alt="" />
+            <ul className={style['ingredients']}>ingredients:
             {ingredients.map(ingredient => (
                 <li>{ingredient.text}</li>
             ))}
-            </ol>
-            <img src={imgSRC} alt="" />
+            </ul>
+            <a className={style['link']} href={link} target="_blank">Open Recipe Details</a>
         </div>
     )
 }
+
 
 export default Recipe
